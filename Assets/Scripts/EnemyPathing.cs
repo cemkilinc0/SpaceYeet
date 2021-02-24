19038627fa4dc9d36190f6c194ef4d6353de1404
cemkilinc0 <cemkilinc0@gmail.com>
2021-02-24 13:38:55 +0300

    Path for enemy ships. Let'em move around!

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 10f;
+    int wayPointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[wayPointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (wayPointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[wayPointIndex].transform.position;
+            var movementPerFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementPerFrame);
+
+            if (transform.position == targetPosition)
+            {
+                wayPointIndex++;
+            }
+        }
+        else
+            Destroy(gameObject);
+    }
+}
